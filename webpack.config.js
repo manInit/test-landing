d82e2b1dc5594bdbdbc2fd511e119d13120e19b2
manInit <info.denisbronnikov@gmail.com>
2022-04-26 14:55:17 +0800

    prepared tools for dev and create project structure

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+
+const target = 'browserslist'
+const mode = process.env.NODE_ENV
+let isDev = mode === 'development'
+
+const minimizer = []
+const plugins = [
+	new MiniCssExtractPlugin({
+		filename: 'styles.[hash].css'
+	}),
+	new HtmlWebpackPlugin({
+		template: './src/index.pug',
+		inject: 'body',
+		minify: false
+	})
+];
+
+if (isDev) {
+	plugins.push(new ESLintPlugin({
+		extensions: ['js']
+	}))
+} else {
+	minimizer.push(new ImageMinimizerPlugin({
+		minimizer: {
+			implementation: ImageMinimizerPlugin.imageminMinify,
+			options: {
+				plugins: [
+					['gifsicle', { interlaced: true }],
+					['jpegtran', { progressive: true }],
+					['optipng', { optimizationLevel: 5 }],
+				]
+			}
+		}
+	}))
+}
+
+module.exports = {
+	mode: mode,
+	target: target,
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.[hash].js',
+		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[hash][ext][query]',
+		clean: true
+	},
+	module: {
+		rules: [
+			{
+        test: /\.pug$/,
+        use: {
+					loader: 'pug3-loader'
+				}
+      },
+			{
+				test: /\.(s[ac]|c)ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: ''
+						} 
+					}, 
+					'css-loader', 
+					'postcss-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset/resource'
+			},
+			{
+				test: /\.(ttf|woff2?|eon|otf)$/,
+				type: 'asset/resource',
+			
+			}
+		]
+	},
+	optimization: {
+		minimizer
+	},
+	plugins,
+	resolve: {
+		extensions: ['.js']
+	},
+	devtool: isDev ? 'source-map' : false,
+	devServer: {
+		static: './',
+		open: true,
+		hot: true
+	}
+}
